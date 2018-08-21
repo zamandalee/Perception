fe2b027f736b9dfe32da6486805b04ea8d570a41
Amanda <a@zalee.me>
2018-08-20 21:32:09 -0700

    Add circle class (moving all from old Perception version)

---
+++ js/circle.js
@@ -0,0 +1,18 @@
+class Circle {
+  constructor(x, y, color, options) {
+    this.x = x;
+    this.y = y;
+    this.color = color;
+    this.radius = options.radius;
+  }
+
+  draw(ctx) {
+    console.log("draw circle");
+    ctx.beginPath();
+    ctx.fillStyle = this.color;
+    ctx.arc( this.x, this.y, this.radius, 0, 2 * Math.PI, true );
+    ctx.fill();
+  }
+}
+
+export default Circle;
